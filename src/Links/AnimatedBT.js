2fcbebd320edcd366bc798044533ae0ba1d2d9b9
dalibor kozar <kozardalibor@gmail.com>
2022-01-07 23:22:08 +0100

    first anim text up and down link

---
+++ src/Links/AnimatedBT.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+const LinkStyle = styled.a`
+    background: linear-gradient(to bottom, #67B7D1 0%, #67B7D1 100%);
+    background-position: 0 100%;
+    background-repeat: repeat-x;
+    background-size: 4px 4px;
+    text-decoration: none;
+    transition: background-size .3s;
+        &:hover { 
+            background-size: 4px 35px;
+        };
+    cursor: pointer;
+`;
+
+const Pstyle = styled.p`
+    font-size: 30px;
+`
+   
+
+const AnimatedBT = () => {
+    return (
+        <>
+        <Pstyle>Lorem ipsum dolor <LinkStyle>nisi ut aliquip</LinkStyle> perspiciatis</Pstyle>
+        </>
+    )
+}
+
+export default AnimatedBT
---
+++ src/Pages/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import AnimatedBT from '../Links/AnimatedBT'
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1>Text</h1>
+            <AnimatedBT />
+            <h1>Buttons</h1>
+        </div>
+    )
+}
+
+export default HomePage
